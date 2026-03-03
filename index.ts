313309fbe1c48cf4058cce454937bd1536642ac9
VIKRAM <vikramria195@gmail.com>
2026-03-04 01:29:11 +0530

    Server Run on Port 3000

--- index.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { PrismaPg } from "@prisma/adapter-pg";
-import {PrismaClient} from '@prisma/client';
-
-const adapter = new PrismaPg();
-const prisma = new PrismaClient({ adapter });
